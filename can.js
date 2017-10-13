877d0f996447e3a538f5e711cbd84375c2a25fa4
sgreylyn <seth.greylyn@gmail.com>
2017-10-13 00:38:01 -0700

    Extracted and generalised Mandelbrot functionality to Julia sets; added colour by HSB

--- can.js
+++ can.js
@@ -2,27 +2,42 @@
 function setup(){
     createCanvas(640, 640);
     pixelDensity(1);
+    colorMode(HSB, 1);
 }
 
 function draw() {
     loadPixels();
+    var x = -0.8;
+    var y = 0.156;
+    basicJuliaSetPixels(x, y);
+    updatePixels();
+ }
 
+ /**
+  * Runs over the pixels in the canvas, entering each one as a complex input z to the function f(z) = z^2 + c,
+  * which is then iterated. If no inputs are provided, the algorithm draws the Mandelbrot set.
+  * @param {float} realComp The real component of the complex number c to use during iteration; if omitted, use the initial 
+  * x-coordinate of the current pixel.
+  * @param {float} imagiComp The imaginary component of the complex number c to use during iteration; if omitted, use the
+  * initial y-coordinate of the current pixel.
+  */
+ function basicJuliaSetPixels(realComp, imagiComp) {
     var mapMin = -1.5; // The minimum range of the pixel map
     var mapMax = 1.5; // The maximum range of the pixel map
     var iterLimit = 100; // The maximum number of iterations to put each pixel through
     var squareMagBound = 36; // If the square of the magnitude of an output point exceeds this value, we assume the value diverges.
 
-    // Loop through all of the pixels, converting them to a complex number, and perform the Mandelbrot process to that number.
+    // Loop through all of the pixels, converting them to a complex number, and perform the function iteration to that number.
     for (var x = 0; x < width; ++x){
         for (var y = 0; y < height; ++y) {
             // Map the pixel coordinates to a complex number a + bi within the box delimited by mapMin and mapMax.
             var a = map(x, 0, width, mapMin, mapMax);
             var b = map(y, 0, height, mapMin, mapMax);
-            // Preserve the initial values of a and b, as they are added at each stage of iteration.
-            var pixelA = a;
-            var pixelB = b;
 
-            // Iterate through the Mandelbrot process, starting with c = a + bi and z_0 = 0, and calculating z_i = (z_{i-1})^2 + c.
+            var fixedA = a; // For fallback use if realComp is not defined.
+            var fixedB = b; // For fallback use if imagiComp is not defined.
+
+            // Iterate through the function, starting with c = a + bi and z_0 = 0, and calculating z_i = (z_{i-1})^2 + c.
             // Note that (a + bi)^2 = a^2 - b^2 + 2abi for *any* complex number a + bi.
 
             // Keep track of how many iterations have taken place for a particular pixel point. Recall that the first iteration of
@@ -32,36 +47,33 @@ function draw() {
             for (var i = 1; i < iterLimit; ++i) {
                 var aSq = a * a;
                 var bSq = b * b;
-                var twoAb = 2*a*b;
-                // We now have the new value, which we add to the original pixel value coordinate-wise.
-                a = pixelA + aSq - bSq;
-                b = pixelB + twoAb;
-            
                 // If the new value has a square-magnitude outside of the bounds, break out of the loop.
-                if(abs(a*a + b*b) > squareMagBound) {
+                if(abs(aSq + bSq) > squareMagBound) {
                     break;
                 }
+                var twoAb = 2*a*b;
+                // We now have the new value, which we add to the inputs coordinate-wise.
+                a = realComp !== void 0? realComp + aSq - bSq : fixedA + aSq - bSq;
+                b = imagiComp !== void 0? imagiComp + twoAb : fixedB + twoAb;
+            
                 // Increment the loop count and continue.
                 ++iterCount;
             }
-            
-            // Map the iterCount to a value between 0 and 1, and then re-map the square root of this number between 0 and 255.
-            var bright = map(iterCount, 0, iterLimit, 0, 1);
-            bright = map(sqrt(bright), 0, 1, 0, 255);
+
+            // Map the iterCount to a value between 0 and 255.
+            var hu = map(iterCount, 1, iterLimit, 0, 1);
+            var col = color(hu, 255, 255);
             // If we reached iterLimit, we assume the starting point does not diverge (i.e., that it's part of the Mandelbrot set),
             // and so we make the brightness zero.
             if (iterCount === iterLimit) {
-                bright = 0;
+                col = color(0);
             }
 
-            // We alter the pixel's RGB value in a somewhat arbitrary fasion; the author likes green, so we've cut the R and B values
-            // down by iterCount. (We can do this because iterCount >= 1.)
             var pix = (x + y * width) * 4;
-            pixels[pix + 0] = bright/iterCount;
-            pixels[pix + 1] = bright;
-            pixels[pix + 2] = bright/iterCount;
+            pixels[pix + 0] = red(col);
+            pixels[pix + 1] = green(col);
+            pixels[pix + 2] = blue(col);
             pixels[pix + 3] = 255;
         }
     }
-    updatePixels();
  }
